222f9631ab71faf1e1f7f652def6795684af5f6f
Rohit-108 <rohitsharma1082001@gmail.com>
2024-09-12 23:54:51 +0530

    VideoContainer add dynamic routes

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -7,7 +7,7 @@ const VideoCard = ({ info }) => {
 
     return (
         <>
-            <div className="p-2 m-2 w-72 shadow-lg">
+            <div className="p-2 m-2 w-72 shadow-lg ">
                 <img className="rounded-lg" alt="Videos" src={thumbnails.medium.url} />
                 <ul>
                     <li className="font-bold py-2">{title}</li>
