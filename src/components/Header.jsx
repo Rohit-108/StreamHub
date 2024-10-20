ca6a279b9e19786b8f3a366dbff20131ba9fd901
Rohit-108 <rohitsharma1082001@gmail.com>
2024-10-20 23:55:44 +0530

    Youtube search Api

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -65,7 +65,7 @@ const Header = () => {
                 <div className="col-span-10 px-10 
                 ">
                     <div>
-                        <input type=" text" className="h-10 w-1/2 border border-gray-400 p-2 rounded-l-full" value={searchQuery}
+                        <input type=" text" className="h-10 w-1/2 border border-gray-400 p-2 rounded-l-full px-3" value={searchQuery}
                             onChange={(e) => setSearchQuery(e.target.value)}
                             onFocus={() => setShowSuggestions(true)}
                             onBlur={() => setShowSuggestions(false)}
@@ -73,7 +73,7 @@ const Header = () => {
                         <button className="h-10 border border-gray-400 px-5 p-2  rounded-r-full bg-gray-200 "><IoSearchOutline /></button>
                     </div>
                     {showSuggestions && (
-                        <div className="fixed bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100">
+                        <div className=" bg-white py-2 px-2 w-[37rem] shadow-lg rounded-lg border border-gray-100 absolute">
                             <ul>
                                 {suggestions.map((s) => (
                                     <li key={s} className="shadow-sm px-3 py-2 hover:bg-gray-100">{s}</li>
