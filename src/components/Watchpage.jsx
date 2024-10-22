329b03a1750eb32b42f93811743f3ab1f1b9af5d
Rohit-108 <rohitsharma1082001@gmail.com>
2024-10-22 23:53:43 +0530

    create comment section

--- src/components/Watchpage.jsx
+++ src/components/Watchpage.jsx
@@ -2,7 +2,7 @@ import { useEffect } from "react"
 import { useDispatch } from "react-redux"
 import { closeMenu } from "./utills/appSlice"
 import { useSearchParams } from "react-router-dom"
-
+import CommentSection from "./CommentSection"
 
 
 const Watchpage = () => {
@@ -17,10 +17,14 @@ const Watchpage = () => {
     }, [])
 
     return (
-        <div className="px-5 m-2">
-            <iframe width="1000" height="500" src={"https://www.youtube.com/embed/" + searchParams.get("v")}
-                title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <div className="flex flex-column">
+            <div className="px-5 m-2">
+                <iframe width="1000" height="500" src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+                    title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            </div>
+            <CommentSection />
         </div>
+
     )
 }
 
