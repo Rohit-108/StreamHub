313c57ff5efbe7c3192199ca889aa07cc6005312
Rohit-108 <rohitsharma1082001@gmail.com>
2024-10-25 01:28:38 +0530

    build live comment button to chat live

--- src/components/LiveChat.jsx
+++ src/components/LiveChat.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import ChatMessage from "./ChatMessage"
 import { useDispatch, useSelector } from "react-redux"
 import { addMessage } from "./utills/chatSlice";
@@ -9,6 +9,7 @@ import { generateRandomName, makeRandomMessage } from "./utills/helper";
 
 
 const LiveChat = () => {
+    const [liveMessage, setLiveMessage] = useState("")
     const dispatch = useDispatch();
     const chatMessages = useSelector((store) => store.chat.messages)
 
@@ -24,7 +25,7 @@ const LiveChat = () => {
                     message: makeRandomMessage(20),
                 })
             )
-        }, 500)
+        }, 2000)
 
         return () => {
             clearInterval(i)
@@ -33,11 +34,34 @@ const LiveChat = () => {
 
 
     return (
-        <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
-            {chatMessages.map((c, index) => (
-                <ChatMessage key={index} name={c.name} message={c.message} />
-            ))}
-        </div>
+        <>
+            <div className="flex flex-col w-full">
+                <div className="w-full h-[500px] ml-2 p-2 border border-black bg-slate-100 rounded-lg overflow-y-scroll flex flex-col-reverse">
+                    <div>
+                        {chatMessages.map((c, index) => (
+                            <ChatMessage key={index} name={c.name} message={c.message} />
+                        ))}
+                    </div>
+                </div>
+                <form className="flex w-full mt-2 ml-2 p-2 border border-black rounded-lg" onSubmit={(e) => {
+                    e.preventDefault();
+                    dispatch(
+                        addMessage({
+                            name: "Rohit Kumar",
+                            message: liveMessage,
+                        })
+                    )
+                    setLiveMessage("")
+                }}>
+                    <input className="w-full px-2" type="text" value={liveMessage} onChange={(e) => {
+                        setLiveMessage(e.target.value)
+                    }} />
+                    <button className="px-2 mx-2 bg-green-100 rounded-md font-bold">Send</button>
+                </form>
+            </div>
+
+        </>
+
     )
 }
 
--- src/components/utills/chatSlice.jsx
+++ src/components/utills/chatSlice.jsx
@@ -11,7 +11,7 @@ const chatSlice = createSlice({
     {
         addMessage: (state, action) => {
             state.messages.splice(OFFSET_LIVE_CHAT, 1)
-            state.messages.unshift(action.payload);
+            state.messages.push(action.payload);
         },
     },
 });
