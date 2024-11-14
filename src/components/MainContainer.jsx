7f86b34292ffd9cf2d1f9317770d28fbe0d33db3
Rohit-108 <rohitsharma1082001@gmail.com>
2024-11-14 23:58:26 +0530

    Sidebar fix

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -3,7 +3,7 @@ import VideoContainer from "./VideoContainer";
 
 const MainContainer = () => {
     return (
-        <div className="col-span-11 p-4 sm:p-6 md:p-8 lg:p-10">
+        <div className="col-span-11 p-4 ">
             <ButtonList />
             <VideoContainer />
         </div>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { useSelector } from "react-redux"
 import { Link } from "react-router-dom";
+import { MdHome } from "react-icons/md";
 
 const Sidebar = () => {
 
@@ -12,29 +13,32 @@ const Sidebar = () => {
 
 
     return (
-        <div className="p-5 shadow-lg w-48 ">
+        <div className="p-5 shadow-lg w-[230px] ">
 
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li>Shorts</li>
-                <li>Videos</li>
-                <li>Live</li>
+            <ul className="w-[300px] pl-12">
+                <Link to="/" >
+                    <div className="flex justify-around px-2  h-5 w-16 x-2 mb-4 text-lg hover:bg-gray-300 hover:font-300 cursor-pointer "><MdHome className="h-5 w-5" />
+                        <li >Home </li>
+                    </div></Link>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Shorts</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Videos</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Live</li>
 
             </ul>
             <h1 className="font-bold pt-5">Subscriptions</h1>
-            <ul>
-                <li>Music</li>
-                <li>Sports</li>
-                <li>Gaming</li>
-                <li>Movies</li>
+            <ul className="w-[300px] pl-12">
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Music</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Sports</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Gaming</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Movies</li>
 
             </ul>
             <h1 className="font-bold pt-5">Watch Later</h1>
-            <ul>
-                <li>Music</li>
-                <li>Sports</li>
-                <li>Gaming</li>
-                <li>Movies</li>
+            <ul className="w-[300px] pl-12">
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Music</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Sports</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Gaming</li>
+                <li className="h-5 w-12 gap-y-5 text-lg hover:bg-gray-300 cursor-pointer">Movies</li>
 
             </ul>
         </div>
